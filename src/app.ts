e293594796cb55fccb63d803e70f03d6a8236bae
munir <smsirajulmonir@gmail.com>
2025-06-11 20:37:54 +0600

    basic server setup ready

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from "express";
+import cors from 'cors';
+
+const app:Application = express();
+app.use(cors());
+
+app.get('/',(req:Request, res:Response) =>{
+    res.send({
+        Message:"Health Care Server is running..."
+    })
+});
+
+export default app;
